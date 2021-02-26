627e53048d97dd6476d61bba721da567e034b030
tomomi94 <kurochan1004@gmail.com>
2021-02-26 15:31:16 +0900

    first commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,58 @@
+class PrototypesController < ApplicationController
+  before_action :authenticate_user!, except: [:show, :index]
+  before_action :set_prototype, only: [:destroy, :update, :edit, :show]
+  before_action :contributor_prototype, only: [:edit, :update, :destroy]
+
+  def index
+    @prototypes = Prototype.includes(:user)
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path(@prototype)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @comment = Comment.new
+    @comments = @prototype.comments
+  end
+
+  def edit
+  end
+
+  def update
+    if @prototype.update(prototype_params)
+       redirect_to prototype_path
+    else
+      render edit
+    end
+  end
+
+  def destroy
+    @prototype.destroy
+    redirect_to root_path(@prototype)
+  end
+
+  private
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def contributor_prototype
+    redirect_to root_path unless current_user == @prototype.user
+  end
+
+end
+
